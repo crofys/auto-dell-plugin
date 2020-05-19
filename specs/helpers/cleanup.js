c46c3c4b1c54ab99202d6a9af9c16e772fefd188
徐长剑 <189323200@qq.com>
2020-05-19 11:29:24 +0800

    feat:删除测试脚本

--- specs/helpers/cleanup.js
+++
@@ -1,7 +0,0 @@
-import del from 'del';
-import path from 'path';
-import { cacheDir } from '../../src/paths';
-
-const cleanup = () => del.sync(path.join(cacheDir));
-
-export default cleanup;
--- specs/helpers/slashify.js
+++
@@ -1,9 +0,0 @@
-import slash from 'slash';
-import get from 'lodash/get';
-import set from 'lodash/set';
-
-export default function slashify(obj, propPaths) {
-  propPaths.forEach(propPath => {
-    set(obj, propPath, slash(get(obj, propPath)));
-  });
-}
